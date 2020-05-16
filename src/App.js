04ac2f48cc5b4b2221b5c152e7646e2020c23f88
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-05-16 11:54:36 -0400

    filled in the form a bit more along with some links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Pizza from "./Pizza";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+
+      <Router>
+        <Link to={`/pizza`}>
+          <button>Start Order Here!</button>
+        </Link>
+        <Link to={`/home`}>
+          <button>Home</button>
+        </Link>
+        <Link to={`/help`}>
+          <button>Help</button>
+        </Link>
+        <Route path="/pizza">
+          <Pizza />
+        </Route>
+        <Route path="/"></Route>
+      </Router>
     </>
   );
 };
